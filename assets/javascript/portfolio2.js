26f651b99e43664b09d748297fb059fb66616e3b
k8blanco <katiewhite303@gmail.com>
2019-03-10 15:52:49 -0600

    mobile-first design, initial commit

---
+++ assets/javascript/portfolio2.js
@@ -0,0 +1,9 @@
+//Portfolio 2
+
+//Initialize SideNav, Floating Action Button
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.fixed-action-btn').floatingActionButton();
+    $(".dropdown-trigger").dropdown();
+  });
+  
