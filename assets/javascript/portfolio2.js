b7c208cd4a770125b64af4cd13765534de8faf05
k8blanco <katiewhite303@gmail.com>
2019-03-10 21:15:35 -0600

    style good, carousel needs button

--- assets/javascript/portfolio2.js
+++ assets/javascript/portfolio2.js
@@ -5,5 +5,21 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.fixed-action-btn').floatingActionButton();
     $(".dropdown-trigger").dropdown();
+    $('.carousel').carousel({
+      onCycleTo: function(data) {
+        let demoLink = $("#" + data.id).attr("href");
+        $("#demoBtn").attr("href", demoLink);
+        console.log();
+      }
+    });
   });
   
+
+  $(".carousel.carousel-slider").carousel({
+    fullWidth: true,
+    indicators: true
+  });
+  
+  // $("$demo").on("click", function(){
+
+  // };
